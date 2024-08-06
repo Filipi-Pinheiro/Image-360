85d25aaeb8931bfbd00ec14eccacc847e074514b
Filipi de Lima <danielelima0908@gmail.com>
2024-08-06 15:41:44 -0300

    update files

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(".container")
+const image = document.querySelector(".image-car")
+
+const cursor = {
+  isDragging : false,
+  initialPosition : 0
+}
+
+let currentImage = 1
+
+const updateImage = (direction) => {
+  direction < 0 ? currentImage == 12 ? currentImage = 1 : currentImage += 1 : currentImage == 1 ? currentImage = 12 : currentImage -= 1
+
+  image.src = `src/imagens/${currentImage}.jpg`
+}
+
+container.addEventListener('mousedown', (event) => {
+  cursor.isDragging = true
+  cursor.initialPosition = event.clientX
+})
+
+container.addEventListener('mouseup', () => {
+  cursor.isDragging = false
+})
+
+container.addEventListener('mousemove', ({ clientX }) => {
+  if(!cursor.isDragging) return;
+
+  const offSet = cursor.initialPosition - clientX
+
+  if(Math.abs(offSet) >= 50) {
+    updateImage(offSet)
+    cursor.initialPosition = clientX
+  }
+})
